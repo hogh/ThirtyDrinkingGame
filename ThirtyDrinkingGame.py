59dc6090b564b2915542aa5ae39e18cc9b79ebd7
Kasper Høgh <kasperhhogh@gmail.com>
2018-11-17 00:40:44 +0100

    added servercode to main pyfile

--- ThirtyDrinkingGame.py
+++ ThirtyDrinkingGame.py
@@ -7,6 +7,22 @@ def onClientChoice():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((IPaddr,port))
 
+def onServerChoice():
+    HOST = ''
+    PORT = 55555
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
 
 
 onClientChoice()
